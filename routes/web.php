8fca6a08bfd77311eb97073d07513ef12aefad0d
Ngọc Hùng <huynhngochung12092001@gmail.com>
2025-03-11 00:33:31 +0700

    refactor: apply global `SetLocale` middleware

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LocaleController;
-use Common\App\Http\Middleware\SetLocale;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(SetLocale::class)->group(function(): void {
-    Route::get('/', HomeController::class)->name('home');
-    Route::put('/locale', LocaleController::class)->name('locale.set');
-});
+Route::get('/', HomeController::class)->name('home');
+Route::put('/locale', LocaleController::class)->name('locale.set');
